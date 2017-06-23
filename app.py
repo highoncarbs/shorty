e2351eee8ed4a60238d1a272d62f2be3ba072c58
Padam Sethia <padamsethia5@gmail.com>
2017-06-23 12:59:55 +0530

    Link Gen Built

--- app.py
+++ app.py
@@ -1,30 +1,78 @@
 from flask import Flask , request , redirect , render_template
 from sqlite3 import OperationalError
-import check_encode
+from check_encode import random_token
+from check_encode import url_check
+import sqlite3
+import sys
+reload(sys)
+sys.setdefaultencoding('UTF-8')
+
+import os
+os.putenv('LANG', 'en_US.UTF-8')
+os.putenv('LC_ALL', 'en_US.UTF-8')
 
 app = Flask(__name__)
+
+shorty_host = "https://localhost:5454/"
 
 # url.db -> root folder * Db check fuction*
 def table_check():
 	create_table = '''
-		CREATE TABLE X_URL(
-		ID INT PRIMARY KEY AUTOINCREMENT,
-		URL TEXT NOT NULL
+		CREATE TABLE WEB_URL(
+		ID INTEGER PRIMARY KEY AUTOINCREMENT,
+		URL TEXT ,
+		S_URL TEXT ,
+		COUNTER INT DEFAULT 0
 		);
 		'''
-	with sqlite3.connect('url.db') as conn:
-		cursor = conn.cursor();
-		try:
-			cursor.execute(create_table)
-		except OperationalError:
-			error = str(OperationalError)
-			pass
+	conn =  sqlite3.connect('url.db')
+	cursor = conn.cursor()
+	try:
+		cursor.execute(create_table)
+		conn.commit()
+		conn.close()
+	except OperationalError:
+		error = str(OperationalError)
+	pass
 
 @app.route('/' , methods=['GET' , 'POST'])
 def index():
 	if request.method == 'POST':
 		og_url = request.form.get('url_input')
-	return render_template('index.html')
+		conn = sqlite3.connect('url.db')
+		cursor = conn.cursor()
+		token_string = random_token()
+		
+		insert_row = """
+			INSERT INTO WEB_URL(URL , S_URL) VALUES('%s' , '%s')
+			"""%(og_url , token_string)
+		print(insert_row)
+		result_cur = cursor.execute(insert_row)
+		conn.commit()
+		conn.close()
+		return render_template('index.html' ,shorty_url = shorty_host+token_string)
+	else:	
+		return render_template('index.html')
+
+# Rerouting funciton
+
+@app.route('/<short_url>')
+def redirect(short_url):
+	conn = sqlite3.connect('url.db')
+	cursor = conn.cursor()
+	select_row = '''
+		SELECT URL FROM WEB_URL WHERE S_URL = %s
+		'''%(short_url)
+	result_cur = cursor.execute(select_row)
+	try:
+		redirect_url = result_cur.fetchnone()[0]
+		conn.commit()
+		conn.close()
+		return redirect(redirect_url)	
+	except Exception as e:
+		error  = e 
+		return render_template('index.html' , error = error)
 
 if __name__ == '__main__':
-	app.run(debug=True)
+	table_check()
+	app.run(port=5454 ,debug=True)
--- check_encode.py
+++ check_encode.py
@@ -7,10 +7,12 @@ and not convert spaces and special characters to HTML chars
 import string
 from math import floor
 from urlparse import urlparse
+import random
 
 BASE_LIST = string.digits + string.letters
 BASE_DICT = dict((c,i) for i,c in enumerate(BASE_LIST)) 
 
+'''
 def encode_to_base62(num , base = 62):
 	if base<0 or base>62:
 		return 0
@@ -29,7 +31,11 @@ def decode_to_base10(num ,base):
 	for i in range(limit):
 		result = base*result + BASE_LIST.find(num[i])
 	return result
+'''
 
+def random_token(size = 6):
+	token = ''.join((random.choice(BASE_LIST)) for char in range(size))
+	return token
 
 def url_check(url):
 	parse = urlparse(url)
@@ -38,5 +44,4 @@ def url_check(url):
 
 	else:
 		# If URL is valid , encode to base64
-		return encode_to_base62(url)
-	
+		return True
